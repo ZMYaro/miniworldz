52c071962ce460b3539161ddb3331d833044ee04
Zachary Yaro <zmyaro@zmyaro.com>
2019-03-27 02:29:41 -0400

    Add comments to console.js and clean up a few things

--- app/scripts/console.js
+++ app/scripts/console.js
@@ -1,22 +1,35 @@
 __editor.console = {
+	/**
+	 * @private
+	 * Add a new message to the bottom of the console output.
+	 * @param {String} message - The message to show
+	 * @param {String} [msgClass] - The CSS class class of the message type
+	 */
 	_addMessage: function (message, msgClass) {
 		var messagePara = document.createElement('p');
-		messagePara.innerText = messagePara.textContent = ('' + message);
+		messagePara.textContent = ('' + message);
 		if (msgClass) {
 			messagePara.className = msgClass;
 		}
+		// Add the new console message to the console.
 		document.getElementById('__consoleOutput').appendChild(messagePara);
+		// Scroll the console to the bottom.
 		document.getElementById('__consoleOutput').scrollTop = Number.MAX_VALUE;
 	},
 	
+	/**
+	 * Initialize the editor console.
+	 */
 	init: function () {
+		// Create the input history list.
 		this._history = [''];
 		this._historyIndex = 0;
+		// Get the user input field.
 		this._inputForm = document.getElementById('__consoleContent').getElementsByTagName('form')[0];
 		this._inputField = this._inputForm.getElementsByTagName('input')[0];
 		
 		// Add event listeners for scrolling through past console inputs.
-		this._inputField.onkeydown = (function (ev) {
+		this._inputField.onkeydown = (ev) => {
 			if (ev.keyCode === 38 && this._historyIndex < this._history.length - 1) {
 				// Up
 				if (this._historyIndex === 0) {
@@ -31,32 +44,64 @@ __editor.console = {
 				this._inputField.value = this._history[this._historyIndex];
 				ev.preventDefault();
 			}
-		}).bind(this);
+		};
 	},
 	
+	/**
+	 * Run the code typed into the console in the MiniWorldZ environment.
+	 * @param {HTMLFormElement} consoleForm - The form element in which the user typed the code
+	 * @param {Event} event - The form submit event
+	 */
 	execute: function (consoleForm, event) {
 		event.preventDefault();
 		event.stopPropagation();
-		this.addHistory(consoleForm.__consoleInput.value);
+		
+		// Get the user input.
+		var input = consoleForm.__consoleInput.value;
+		
+		// Add the typed input to the console's hisotry.
+		this.addHistory(input);
 		this._historyIndex = 0;
-		__env.sandbox.runCode(consoleForm.__consoleInput.value);
+		
+		// Run the code.
+		__env.sandbox.runCode(input);
+		
+		// Clear the console input field.
 		consoleForm.__consoleInput.value = '';
 	},
 	
+	/**
+	 * Clear all the messages from the console.
+	 */
 	clear: function () {
 		document.getElementById('__consoleOutput').innerHTML = '';
 	},
 	
+	/**
+	 * Add a message to the console's output window and input history.
+	 * @param {String} message - The user's input
+	 */
 	addHistory: function (message) {
+		// Show the new message in the console.
 		this._addMessage(message, 'history');
+		// Set the most recent history entry to that text.
 		this._history[0] += message;
+		// Add a new (blank) history entry for the now-empty console input field.
 		this._history.unshift('');
 	},
 	
+	/**
+	 * Show an error message in the console's output window.
+	 * @param {String} message - The error message text
+	 */
 	error: function (message) {
 		this._addMessage(message, 'error');
 	},
 	
+	/**
+	 * Show an unstyled message in the console's output window.
+	 * @param {String} message - The message text
+	 */
 	log: function (message) {
 		this._addMessage(message, false);
 	},
